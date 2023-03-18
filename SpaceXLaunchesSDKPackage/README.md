551b96c86e50e5cc37a79ee29da39e787da7781e
V <v>
2023-03-18 03:23:06 +0100

    Package added

---
+++ SpaceXLaunchesSDKPackage/README.md
@@ -0,0 +1,3 @@
+# SpaceXLaunchesSDKPackage
+
+A description of this package.
---
+++ SpaceXLaunchesSDKPackage/Sources/SpaceXLaunchesSDKPackage/SpaceXLaunchesSDKPackage.swift
@@ -0,0 +1,6 @@
+public struct SpaceXLaunchesSDKPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
